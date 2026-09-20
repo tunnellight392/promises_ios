732b72d85cd4ec798a46400221257f01ee206f10
Alex Therakathu <therualex@gmail.com>
2026-09-19 21:06:11 -0400

    Initial Commit

---
+++ Promises/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Promises
+//
+//  Created by Alex Therakathu on 9/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Promises/PromisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromisesApp.swift
+//  Promises
+//
+//  Created by Alex Therakathu on 9/19/26.
+//
+
+import SwiftUI
+
+@main
+struct PromisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
